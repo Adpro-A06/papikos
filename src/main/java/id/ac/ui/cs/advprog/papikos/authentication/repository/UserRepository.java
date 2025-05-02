8cec819988d68f7767023877750f32aa521ef054
Stefanus Tan Jaya <stefanus.tan@ui.ac.id>
2025-05-02 20:11:07 +0700

    feat: Add approvePemilikKos for admin

--- src/main/java/id/ac/ui/cs/advprog/papikos/authentication/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/authentication/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package id.ac.ui.cs.advprog.papikos.authentication.repository;
 
+import id.ac.ui.cs.advprog.papikos.authentication.model.Role;
 import id.ac.ui.cs.advprog.papikos.authentication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+    List<User> findByRoleAndApprovedFalse(Role role);
 }
