e101c5397045bd3c576690405fc90119c0d8b45b
cilla09 <priscilla.natanael@ui.ac.id>
2025-05-02 19:32:33 +0700

    [RED] Add tests for MessageRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/chat/repository/MessageRepositoryTest.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.papikos.chat.repository;
+
+import id.ac.ui.cs.advprog.papikos.chat.model.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MessageRepositoryTest {
+
+    private MessageRepository messageRepository;
+
+    @BeforeEach
+    void setUp() {
+        messageRepository = new MessageRepositoryImpl();
+
+        Message message = new Message();
+        message.setChatroomId(1L);
+        message.setSenderId(101L);
+        message.setContent("Hello!");
+        message.setTimestamp(LocalDateTime.of(2023, 1, 1, 10, 0));
+
+        messageRepository.save(message);
+    }
+
+    @Test
+    void testFindMessagesByChatroomId() {
+        List<Message> result = messageRepository.findByChatroomIdOrderByTimestampAsc(1L);
+        assertEquals(1, result.size());
+        assertEquals("Hello!", result.get(0).getContent());
+        assertEquals(101L, result.get(0).getSenderId());
+    }
+}
