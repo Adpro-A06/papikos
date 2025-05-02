eaff4a9762117cd0f8b978e9ec524797058e59b3
sashimi04 <emailkamu@gmail.com>
2025-05-02 20:37:36 +0700

    [GREEN] Implement WishlistController 25% implementation

--- src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/controller/WishlistController.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/controller/WishlistController.java
@@ -5,8 +5,10 @@ import id.ac.ui.cs.advprog.papikos.wishlist.service.WishlistService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/wishlists")
+@RequestMapping("/wishlist")
 public class WishlistController {
 
     private final WishlistService wishlistService;
@@ -22,5 +24,30 @@ public class WishlistController {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Wishlist>> getAllWishlists() {
+        List<Wishlist> all = wishlistService.getAllWishlists();
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Wishlist> getWishlistById(@PathVariable int id) {
+        Wishlist wishlist = wishlistService.getWishlistById(id);
+        if (wishlist == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(wishlist);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWishlistById(@PathVariable int id) {
+        boolean deleted = wishlistService.deleteWishlistById(id);
+        if (deleted) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
