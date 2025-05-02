7823fe1d9bc8fd2364fc53229e7d64367c8ab669
cilla09 <priscilla.natanael@ui.ac.id>
2025-05-02 20:00:21 +0700

    [GREEN] Implement ChatroomService

---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/chat/service/ChatroomService.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.papikos.chat.service;
+
+import id.ac.ui.cs.advprog.papikos.chat.model.Chatroom;
+import id.ac.ui.cs.advprog.papikos.chat.repository.ChatroomRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ChatroomService {
+
+    private final ChatroomRepository chatroomRepository;
+
+    public ChatroomService(ChatroomRepository chatroomRepository) {
+        this.chatroomRepository = chatroomRepository;
+    }
+
+    public Chatroom createChatroom(Long renterId, Long ownerId, Long propertyId) {
+        Optional<Chatroom> existingChatroom = chatroomRepository
+                .findByRenterIdAndOwnerIdAndPropertyId(renterId, ownerId, propertyId);
+
+        if (existingChatroom.isPresent()) {
+            return existingChatroom.get();
+        }
+
+        Chatroom chatroom = new Chatroom();
+        chatroom.setRenterId(renterId);
+        chatroom.setOwnerId(ownerId);
+        chatroom.setPropertyId(propertyId);
+        chatroom.setCreatedAt(LocalDateTime.now());
+
+        return chatroomRepository.save(chatroom);
+    }
+}
