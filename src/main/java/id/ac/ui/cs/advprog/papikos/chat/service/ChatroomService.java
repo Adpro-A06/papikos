4d28f5606bfe3ea7760d5fa006c9b17d4d7c98c8
cilla09 <priscilla.natanael@ui.ac.id>
2025-05-09 16:44:52 +0700

    [GREEN] make ChatroomService interface and implement ChatroomServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/papikos/chat/service/ChatroomService.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/chat/service/ChatroomService.java
@@ -1,44 +1,11 @@
 package id.ac.ui.cs.advprog.papikos.chat.service;
 
 import id.ac.ui.cs.advprog.papikos.chat.model.Chatroom;
-import id.ac.ui.cs.advprog.papikos.chat.repository.ChatroomRepository;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class ChatroomService {
-
-    private final ChatroomRepository chatroomRepository;
-
-    public ChatroomService(ChatroomRepository chatroomRepository) {
-        this.chatroomRepository = chatroomRepository;
-    }
-
-    public Chatroom createChatroom(Long renterId, Long ownerId, Long propertyId) {
-        Optional<Chatroom> existingChatroom = chatroomRepository
-                .findByRenterIdAndOwnerIdAndPropertyId(renterId, ownerId, propertyId);
-
-        if (existingChatroom.isPresent()) {
-            return existingChatroom.get();
-        }
-
-        Chatroom chatroom = new Chatroom();
-        chatroom.setRenterId(renterId);
-        chatroom.setOwnerId(ownerId);
-        chatroom.setPropertyId(propertyId);
-        chatroom.setCreatedAt(LocalDateTime.now());
-
-        return chatroomRepository.save(chatroom);
-    }
-
-    public List<Chatroom> getChatroomsByRenterId(Long renterId) {
-        return chatroomRepository.findByRenterId(renterId);
-    }
-
-    public List<Chatroom> getChatroomsByOwnerId(Long ownerId) {
-        return chatroomRepository.findByOwnerId(ownerId);
-    }
+public interface ChatroomService {
+    Chatroom createChatroom(Long renterId, Long ownerId, Long propertyId);
+    Chatroom getChatroomById(Long id);
+    List<Chatroom> getChatroomsByRenterId(Long renterId);
+    List<Chatroom> getChatroomsByOwnerId(Long ownerId);
 }
