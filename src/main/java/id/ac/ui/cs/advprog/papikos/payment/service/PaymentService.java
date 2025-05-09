98891c6e4aa9a7841c13f521e5f19bf967d1b299
Malvin <malvinmraqin@gmail.com>
2025-05-09 20:11:35 +0700

    [REFACTOR]: Refactoring Payment Service

--- src/main/java/id/ac/ui/cs/advprog/papikos/payment/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/payment/service/PaymentService.java
@@ -1,10 +1,17 @@
 package id.ac.ui.cs.advprog.papikos.payment.service;
 
+import id.ac.ui.cs.advprog.papikos.payment.model.Payment;
+import id.ac.ui.cs.advprog.papikos.payment.model.TransactionType;
+
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentService {
     void topUp(UUID userId, BigDecimal amount);
-    BigDecimal getBalance(UUID userId);
     void pay(UUID fromUserId, UUID toUserId, BigDecimal amount);
+    BigDecimal getBalance(UUID userId);
+    List<Payment> getUserTransactions(UUID userId);
+    List<Payment> filterTransactions(UUID userId, LocalDate startDate, LocalDate endDate, TransactionType type);
 }
