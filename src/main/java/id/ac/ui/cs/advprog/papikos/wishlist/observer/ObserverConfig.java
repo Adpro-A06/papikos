a8dccdad6fac7612526c4fcbe0db5579841b9e79
sashimi04 <liliumroses0@gmail.com>
2025-05-25 20:56:53 +0700

    Update ObserverConfig.java

--- src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/observer/ObserverConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/observer/ObserverConfig.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.papikos.wishlist.observer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 
 @Configuration
