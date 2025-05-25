a7169ae83240474018a6f623ad883e9a181f83ff
sashimi04 <liliumroses0@gmail.com>
2025-05-25 20:38:30 +0700

    Add ObserverConfig.java to register all observer

---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/wishlist/observer/ObserverConfig.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.papikos.wishlist.observer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Configuration
+public class ObserverConfig {
+
+    @Autowired
+    public ObserverConfig(WishlistNotifier notifier, List<WishlistObserver> observers) {
+        observers.forEach(notifier::addObserver);
+    }
+}
